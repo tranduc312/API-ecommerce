7f93b8352f65f9108836a5a7b98e973a5fccdc18
tranduc312 <tranvanduc31298@gmail.com>
2022-06-17 11:40:02 +0700

    first commit

---
+++ src/main/java/com/springboot/ecommerce/springbootecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,56 @@
+package com.springboot.ecommerce.springbootecommerce.config;
+
+import com.springboot.ecommerce.springbootecommerce.entity.Product;
+import com.springboot.ecommerce.springbootecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportActions));
+        
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // get all entity
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
---
+++ src/main/java/com/springboot/ecommerce/springbootecommerce/entity/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.springboot.ecommerce.springbootecommerce.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
